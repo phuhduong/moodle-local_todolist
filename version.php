4a174192f87bb221597fc55b4fcd1560f9c0e1be
Phu Duong <phuhoang.duong@nashtechglobal.com>
2025-07-01 09:27:58 +0700

    revised version.php info

--- version.php
+++ version.php
@@ -1,8 +1,10 @@
 <?php
-global $plugin;
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_todolist';
-$plugin->version = '2025062500';    // YYYMMDDXX
-$plugin->requires = '2025041400';   // Moodle 5.0.0 minimum
+$plugin->version = '2025062500';
+$plugin->requires = '2022041900';
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '0.1';
+
 $plugin->stylesheet = 'styles.css';
